7b5a57c1fbfb99c916cf7bb37c3046df905eca51
Arllain <arllain@gmail.com>
2021-08-21 09:27:58 -0300

    IBOutlets da tela GameViewController

--- MyGames/MyGames/GameViewController.swift
+++ MyGames/MyGames/GameViewController.swift
@@ -8,7 +8,13 @@
 import UIKit
 
 class GameViewController: UIViewController {
-
+    
+    @IBOutlet weak var lbTitle: UILabel!
+    @IBOutlet weak var lbConsole: UILabel!
+    @IBOutlet weak var lbReleaseDate: UILabel!
+    @IBOutlet weak var ivCover: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
