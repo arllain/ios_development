fd807b3547126747e6f3e6624f6941c872c910e8
Arllain <arllain@gmail.com>
2021-08-21 09:31:43 -0300

    IBOutlets da tela AddEditViewController

--- MyGames/MyGames/AddEditViewController.swift
+++ MyGames/MyGames/AddEditViewController.swift
@@ -8,7 +8,15 @@
 import UIKit
 
 class AddEditViewController: UIViewController {
-
+    
+    @IBOutlet weak var tfTitle: UITextField!
+    @IBOutlet weak var tfConsole: UITextField!
+    @IBOutlet weak var dpReleaseDate: UIDatePicker!
+    @IBOutlet weak var btAddEdit: UIButton!
+    @IBOutlet weak var btCover: UIButton!
+    @IBOutlet weak var ivCover: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
