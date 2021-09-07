0c97edfde7895f5053c0e8e4e033febceb822daa
Arllain <arllain@gmail.com>
2021-09-07 13:46:14 -0300

    Suporte ao Cocoapods e adidcionar Menu Slider com duas telas

--- Carangas/Carangas/Controllers/CarsTableViewController.swift
+++ Carangas/Carangas/Controllers/CarsTableViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import SideMenu
 
 class CarsTableViewController: UITableViewController {
     
@@ -21,6 +22,16 @@ class CarsTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+  
+        // Define the menus
+        SideMenuManager.default.leftMenuNavigationController = storyboard!.instantiateViewController(withIdentifier: "LeftMenuNavigationController") as? SideMenuNavigationController
+        
+        
+        // Enable gestures. The left and/or right menus must be set up above for these to work.
+        // Note that these continue to work on the Navigation Controller independent of the View Controller it displays!
+        SideMenuManager.default.addPanGestureToPresent(toView: self.navigationController!.navigationBar)
+        // Updated
+        SideMenuManager.default.addScreenEdgePanGesturesToPresent(toView: self.navigationController!.view, forMenu: SideMenuManager.PresentDirection.left)
         
         label.text = "Carregando carros ..."
         
